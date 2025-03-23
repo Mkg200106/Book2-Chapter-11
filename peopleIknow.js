432c85da039bff4b1c78c8e2977a7155d1e526f7
Mkg200106 <martharamphele01@gmail.com>
2025-03-23 00:55:51 -0700

    Add files via upload

---
+++ peopleIknow.js
@@ -0,0 +1,18 @@
+let people = ["Mary","Bobby","Judy","Eddie","Herbie","Tony","Bobby"];
+let otherPeople= ["Teddy","Cathy","G-berg","Georgie"];
+document.getElementById("peopleIKnow").innerHTML= people.toString();
+people= people.concat(otherPeople);
+people = people.indexOf("Eddie");
+people = people.join("#");
+people = people.lastIndexOf("Bobby");
+people = people.people.pop();
+people.pop();
+//people = people.push("Teddy");
+//people.push("Teddy");
+//people = people.reverse();
+//people.shift();
+//people.unshift("Tedddy");
+//people = people.slice(0,3);
+//people = people.sort();
+//people.splice(1,0,"Cathy");
+
